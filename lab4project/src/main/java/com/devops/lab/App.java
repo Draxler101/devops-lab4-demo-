ababd6de3150b35dc164d56813bf2d42b4ead3b0
Cloud User <cloud@2021wb86916.com>
2025-07-15 20:06:04 +0530

    Updated App.java Locally

--- lab4project/src/main/java/com/devops/lab/App.java
+++ lab4project/src/main/java/com/devops/lab/App.java
@@ -1,13 +1,10 @@
 package com.devops.lab;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello from Continous Integration trigger " );
+        System.out . printtn( "DevOps Lab 4 completes the first Line output show Stated by SCM");
     }
 }
